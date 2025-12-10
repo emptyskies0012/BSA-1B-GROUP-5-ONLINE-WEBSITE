920c226cd745ce13ec5b7fd2a1ee8b0094588489
emptyskies0012 <aldrinsaliendra@gmail.com>
2025-12-10 08:50:22 +0800

    Add files via upload

---
+++ productpopup.js
@@ -0,0 +1,7 @@
+function openPopup(id) {
+    document.getElementById("popup" + id).style.display = "flex";
+}
+
+function closePopup(id) {
+    document.getElementById("popup" + id).style.display = "none";
+}
